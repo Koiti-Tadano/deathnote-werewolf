8cdf70ea9dc41e5b3e6efe64268d71ed49e98668
Koiti-Tadano <toyanic0311@icloud.com>
2025-08-27 12:44:36 +0900

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-
-// Set your Disqus shortname to enable global anonymous comments.
-// 1) Create a site at https://disqus.com/
-// 2) Put your shortname below, e.g. 'my-disqus-shortname'
-window.SITE_CONFIG = {
-  disqusShortname: '' // <- e.g. 'your-shortname' (leave empty to disable)
-};
