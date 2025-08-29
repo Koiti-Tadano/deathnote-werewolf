687d87f788c4d844c6d9a2029c0be0929c653c3d
Koiti-Tadano <toyanic0311@icloud.com>
2025-08-30 07:56:45 +0900

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,12 +1,12 @@
 // assets/js/Firebase.config.js
 var firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "1:XXXXXXXX:web:XXXXXXXX"
+y: "AIzaSyBtr461rgEf3kfeXCjDUr8de3H1YjDWlNg",
+  authDomain: "deathnote-werewolf.firebaseapp.com",
+  databaseURL: "https://deathnote-werewolf-default-rtdb.firebaseio.com",
+  projectId: "deathnote-werewolf",
+  storageBucket: "deathnote-werewolf.firebasestorage.app",
+  messagingSenderId: "488023525397",
+  appId: "1:488023525397:web:4edf8ac542cffb6e1de61c"
 };
 
 // Firebase初期化（これがないとchat.jsが動かない）
