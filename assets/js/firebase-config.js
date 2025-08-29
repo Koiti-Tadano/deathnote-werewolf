2e5e4251cface275f9e4538c7c9cefeb40332007
Koiti-Tadano <toyanic0311@icloud.com>
2025-08-29 13:33:32 +0900

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,21 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBtr461rgEf3kfeXCjDUr8de3H1YjDWlNg",
-  authDomain: "deathnote-werewolf.firebaseapp.com",
-  projectId: "deathnote-werewolf",
-  storageBucket: "deathnote-werewolf.firebasestorage.app",
-  messagingSenderId: "488023525397",
-  appId: "1:488023525397:web:4edf8ac542cffb6e1de61c",
-  measurementId: "G-03WJMMMSHD"
+// assets/js/Firebase.config.js
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "XXXXXXXX",
+  appId: "1:XXXXXXXX:web:XXXXXXXX"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebase初期化（これがないとchat.jsが動かない）
+firebase.initializeApp(firebaseConfig);
