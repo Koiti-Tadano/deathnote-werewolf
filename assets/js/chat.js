9fdc5e91d0e8a531406d3c18e4a7d4054169b445
Koiti-Tadano <toyanic0311@icloud.com>
2025-09-01 11:13:47 +0900

    Update chat.js

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -153,39 +153,91 @@ playersRef.on("value", (snap) => {
   }
 
   // --- 役職割り当て関数 ---
-  async function assignRoles(roomId) {
-    const roles = [
-      "wolf",       // 人狼1
-      "madman",     // 狂人1
-      "detective",  // 探偵1
-      "villager", "villager", "villager", "villager" // 村人4
-    ];
+async function assignRoles(roomId) {
+  const roles = ["wolf","madman","detective","villager","villager","villager","villager"];
+  const snap = await firebase.database().ref(`rooms/${roomId}/players`).once("value");
+  const players = snap.val() || {};
+  const playerNames = Object.keys(players);
 
-    const snap = await firebase.database().ref(`rooms/${roomId}/players`).once("value");
-    const players = snap.val() || {};
-    const playerNames = Object.keys(players);
+  // シャッフル
+  for (let i = roles.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [roles[i], roles[j]] = [roles[j], roles[i]];
+  }
 
-    // if (playerNames.length !== roles.length) {
-    //   alert(`役職の数(${roles.length})とプレイヤー数(${playerNames.length})が一致しません`);
-    //   return;
-    // }
+  for (let i = 0; i < playerNames.length; i++) {
+    const name = playerNames[i];
+    const role = roles[i] || "villager";
+    const fullName = generateFullName();
+    const profile = generateProfile();
+    const infoCards = generateInfoCard(role, fullName, profile);
 
-    // シャッフル
-    for (let i = roles.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [roles[i], roles[j]] = [roles[j], roles[i]];
-    }
-
-    playerNames.forEach((name, idx) => {
-      firebase.database().ref(`rooms/${roomId}/players/${name}/role`).set(roles[idx] || "villager");
-    });
-
-    firebase.database().ref(`rooms/${roomId}/messages`).push({
-      text: "役職が割り当てられました。",
-      name: "システム",
-      time: Date.now()
+    firebase.database().ref(`rooms/${roomId}/players/${name}`).update({
+      role,
+      fullName,
+      profile,
+      infoCards
     });
   }
+  firebase.database().ref(`rooms/${roomId}/messages`).push({
+    text: "役職とプロフィールが配布されました。",
+    name: "システム",
+    time: Date.now()
+  });
+}
+const fakeSurnames = [
+  "佐原木","神代川","高森田","藤宮堂","北条木","桐沢谷","篠原江","葛城井","綾峰","東雲木",
+  "氷川原","鷹森","葉月川","橘野","秋津原","久遠木","真田沢","花村江","水城田","黒川谷",
+  "白峰","大鳥居","小野森","星川堂","天城井","美濃部","八雲木","九条原","深山田","紫垣",
+  "西園川","榊原井","安曇野","若狭木","羽黒田","桜庭谷","柏崎川","三雲堂","雪村江","沢渡木",
+  "如月谷","朧川","暁森","鬼塚原","葵井","唐沢江","稲城堂","真壁木","月岡川","白鷺田",
+  // 追加分
+  "藤白木","羽生川","真嶋田","桂木沢","宝生谷","新宮原","瑞穂川","玉置谷","笹倉江","小城井",
+  "広瀬木","大槻原","矢島沢","香坂川","成瀬江","水無月","穂高田","庄司木","鵜飼井","東条谷",
+  "黒須川","西森堂","津島田","比良木","大和江","氷室谷","三崎原","藤波田","早瀬木","青柳川",
+  "伊吹原","千早井","鏡原木","緑川谷","御影堂","森永江","榎本木","時任川","冬木沢","長浜谷",
+  "若宮原","篠崎川","鷲尾木","霧島江","真行寺","高嶺沢","藤沢谷","忍野井","美月原","安倍川"
+];
+const givenNames = ["タケシ","ヒロキ","ユウタ","ケンタ","リョウ","ダイチ","ショウタ","ユウジ","マコト","アツシ",
+  "ミカ","サキ","ユイ","カナ","アヤカ","ミホ","ナオミ","リナ","エリ","マユ"];
+
+function generateFullName() {
+  const surname = fakeSurnames[Math.floor(Math.random() * fakeSurnames.length)];
+  const given = givenNames[Math.floor(Math.random() * givenNames.length)];
+  return surname + " " + given;
+}
+const outfits = ["スーツ","ジャージ","着物","白衣","パーカー","セーラー服","作業着"];
+const likes = ["カレー","ゲーム","数学","犬","カラオケ","コーヒー"];
+const dislikes = ["虫","早起き","ピーマン","大人数の集まり"];
+const strengths = ["運動","観察","記憶力","交渉","料理"];
+const weaknesses = ["方向感覚","計算","嘘をつく","体力","集中力"];
+
+function generateProfile() {
+  return {
+    outfit: outfits[Math.floor(Math.random() * outfits.length)],
+    like: likes[Math.floor(Math.random() * likes.length)],
+    dislike: dislikes[Math.floor(Math.random() * dislikes.length)],
+    strong: strengths[Math.floor(Math.random() * strengths.length)],
+    weak: weaknesses[Math.floor(Math.random() * weaknesses.length)]
+  };
+}
+function generateInfoCard(role, fullName, profile) {
+  const cards = [];
+
+  // プロフィールから
+  cards.push(`誰かは ${profile.outfit} を着ている`);
+  cards.push(`誰かは ${profile.like} が好き`);
+  cards.push(`誰かは ${profile.dislike} が嫌い`);
+
+  // 人狼専用ヒント（母音数を検出）
+  if (role === "wolf") {
+    const vowels = fullName.match(/[aiueoアイウエオ]/gi) || [];
+    cards.push(`人狼のフルネームには母音が ${vowels.length} 個含まれている`);
+  }
+
+  return cards;
+}
+  
 
   // --- アクションメニュー ---
   let role = null;
