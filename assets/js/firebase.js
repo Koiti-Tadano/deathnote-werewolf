86917d4e8e2266c54ec9eda31ed37cb91fe179be
Koiti-Tadano <toyanic0311@icloud.com>
2025-09-16 07:58:53 +0900

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,7 +1,13 @@
 // === firebase.js ===
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
 import { getDatabase, ref, set, get, update, remove, onValue, push } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+// assets/js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+import { firebaseConfig } from "./firebase-config.js";
 
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 // Firebase 設定（自分のプロジェクトに置き換えてね！）
 const firebaseConfig = {
   apiKey: "AIzaSyBtr461rgEf3kfeXCjDUr8de3H1YjDWlNg",
